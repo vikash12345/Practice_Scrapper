41bbb619f31e6100d28007c2ce89cab32dc01747
vikash12345 <vikash@backgroundcheck.email>
2016-08-18 14:12:39 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -5,7 +5,7 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 //
 // // Read in a page
-$html = scraperwiki::scrape("http://foo.com");
+$html = scraperwiki::scrape("http://www.mciindia.org/ViewDetails.aspx?ID=1");
 //
 // // Find something on the page using css selectors
 $dom = new simple_html_dom();
@@ -13,10 +13,10 @@ $dom->load($html);
 print_r($dom->find("table.list"));
 //
 // // Write out to the sqlite database using scraperwiki library
-scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
+//scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
 //
 // // An arbitrary query against the database
-scraperwiki::select("* from data where 'name'='peter'")
+//scraperwiki::select("* from data where 'name'='peter'")
 
 ?>
 
