9b47caed10d3b7f282d942d363c41eb723a848e2
vikash12345 <vikash@backgroundcheck.email>
2016-08-22 14:18:39 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,31 +1,23 @@
 <?php
-
 require 'scraperwiki.php';
-require 'scraperwiki/simple_html_dom.php';
-
-	include_once('simple_html_dom.php');
-	$MAX_ID = 2;
-	for($id = 1; $id <= $MAX_ID; $id++)
-	{
-
-	$html = file_get_html("http://www.mciindia.org/ViewDetails.aspx?ID=".$id);
-	  
-echo 	  $name = $html->find('span[id=Name]',0)->plaintext;
-echo	  $fathername = $html->find('span[id="FatherName"]',0)->plaintext;
-echo	  $DOB = $html->find('span[id="DOB"]',0)->plaintext;
-echo	  $lblinfor = $html->find('span[id="lbl_Info"]',0)->plaintext;
-echo	  $reg = $html->find('span[id="Regis_no"]',0)->plaintext;
-echo	  $date_reg = $html->find('span[id="Date_Reg"]',0)->plaintext;
-echo	  $Lbl_Council = $html->find('span[id="Lbl_Council"]',0)->plaintext;
-echo	  $qual = $html->find('span[id="Qual"]',0)->plaintext;
-echo	  $QualYear = $html->find('span[id="QualYear"]',0)->plaintext;
-echo	  $Univ = $html->find('span[id="Univ"]',0)->plaintext;
-echo  	 $Address = $html->find('span[id="Address"]',0)->plaintext;
+    require 'scraperwiki/simple_html_dom.php';
+?>
 
 
-
-
+<?php
+$url="http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm";
+$html = scraperWiki::scrape( $url );
+$dom = new simple_html_dom();
+$dom->load($html);
+foreach($dom->find("div[@align='left'] tr") as $data){
+    $tds = $data->find("td");
+    if(count($tds)==12){
+        $record = array(
+            'country' => $tds[0]->plaintext, 
+            'years_in_school' => intval($tds[4]->plaintext)
+        );
+        print json_encode($record) . "\n";
+        scraperwiki::save( array('country'), $record );
+    }
 }
-
-	
 ?>
